497e92bbfe219e11bc614765d687e4f3c2982756
Jibran-Khot <jibrankhot37@gmail.com>
2026-08-01 21:53:04 +0530

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { EmployeeTopPerformersComponent } from './employee-top-performers/employee-top-performers.component';
+import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [EmployeeTopPerformersComponent]
+    imports: [EmployeeTopPerformersComponent, EmployeeCardComponent]
 })
 export class AppComponent {
 
