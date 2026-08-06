ac942c83d9da4618574a1547be78e562911a15c6
Jibran-Khot <jibrankhot37@gmail.com>
2026-08-06 21:35:56 +0530

    feat: initialize employee objects using constructors

--- src/app/employee-class/employee-class.component.ts
+++ src/app/employee-class/employee-class.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 class Employee {
-  id: number = 0;
-  name: string = '';
-  department: string = '';
-  salary: number = 0;
+  constructor(
+    public id: number,
+    public name: string,
+    public department: string,
+    public salary: number
+  ) { }
 
   getEmployeeSummary(): string {
     return `${this.name} works in ${this.department} department and earns ₹${this.salary}.`;
@@ -19,27 +21,10 @@ class Employee {
 })
 export class EmployeeClassComponent {
 
-  employees: Employee[] = [];
+  employees: Employee[] = [
+    new Employee(1, 'Jibran', 'IT', 50000),
+    new Employee(2, 'Arshan', 'HR', 45000),
+    new Employee(3, 'Shayan', 'Finance', 55000)
+  ];
 
-  constructor() {
-    const emp1 = new Employee();
-    emp1.id = 1;
-    emp1.name = 'Jibran';
-    emp1.department = 'IT';
-    emp1.salary = 50000;
-
-    const emp2 = new Employee();
-    emp2.id = 2;
-    emp2.name = 'Arshan';
-    emp2.department = 'HR';
-    emp2.salary = 45000;
-
-    const emp3 = new Employee();
-    emp3.id = 3;
-    emp3.name = 'Shayan';
-    emp3.department = 'Finance';
-    emp3.salary = 55000;
-
-    this.employees.push(emp1, emp2, emp3);
-  }
 }
